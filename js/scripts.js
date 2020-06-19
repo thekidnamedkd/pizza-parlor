6e511724e18ac29b7c1a4f023e7a318bdc41fc83
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-19 09:02:40 -0700

    establish pizza object constructor and variables for its properties.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+function Pizza (size, sauce, toppings) {
+  this.size = size
+  this.sauce = sauce
+  this.toppings = toppings
+}
+
+let size
+let sauce
+let toppings
+
 $(document).ready(function() {
 event.preventDefault();
 })
