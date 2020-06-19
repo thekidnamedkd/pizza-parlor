6963ff4a1efe69bc58e1ec8c3435c7a92b529c11
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-19 09:18:57 -0700

    build first method for pizza object to establish base price based on size.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,22 @@ function Pizza (size, sauce, toppings) {
   this.toppings = toppings
 }
 
-let size
-let sauce
-let toppings
+Pizza.prototype.price = function() {
+  this.price = 16
+  if (this.size === "small") {
+    this.price += 3
+  } else if (this.size === "medium") {
+    this.price += 6
+  } else if (this.size === "large") {
+    this.price += 9
+  }
+}
 
-$(document).ready(function() {
-event.preventDefault();
-})
+// let size
+// let sauce
+// let toppings
+// let pizza
+
+// $(document).ready(function() {
+// event.preventDefault();
+// })
