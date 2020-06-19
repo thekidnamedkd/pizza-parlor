e80e5970ea3275c456a8aca30eca735882c45b09
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-19 13:57:04 -0700

    add functionality sauce pricing and begin for loop testing for meat toppings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,42 @@
 function PizzaPrice () {
-  this.total = 0
-  this.small = 16
-  this.medium = 19
-  this.large = 22
-  this.oilSauce = 0
-  this.redSauce = 1
-  this.whiteSauce = 2
-  this.addVegTop = 1
-  this.addMeatTop = 3
+  this.total = 0;
+  this.small = 16;
+  this.medium = 19;
+  this.large = 22;
+  this.oilSauce = 0;
+  this.redSauce = 1;
+  this.whiteSauce = 2;
+  this.addMeat = 3;
+  this.addVeg = 1;
 
   if (document.forms[0].size[0].checked == true) {
-    this.total += this.small
+    this.total += this.small;
   } else if (document.forms[0].size[1].checked == true) {
-    this.total += this.medium
+    this.total += this.medium;
   } else if (document.forms[0].size[2].checked === true) {
-    this.total += this.large
+    this.total += this.large;
   }
 
   console.log(this.total)
 
-  if (document.forms[0].sauce[0].checked === "true") {
-    this.total += this.small
-  } else if (document.forms[0].sauce[1].checked === "true") {
-    this.total += this.medium
-  } else if (document.forms[0].sauce[2].checked === "true") {
-    this.total += this.large
+  if (document.forms[0].sauce[0].checked === true) {
+    this.total += this.oilSauce;
+  } else if (document.forms[0].sauce[1].checked === true) {
+    this.total += this.redSauce;
+  } else if (document.forms[0].sauce[2].checked === true) {
+    this.total += this.whiteSauce;
   }
-  return PizzaPrice
-}
+  console.log(this.total);
   
-  console.log(this.total)
-//   if (this.sauce === "true") {
-//     this.total += this.oilSauce
-//   } else if (this.sauce === "true") {
-//     this.total += this.redSauce
-//   } else if (this.sauce === "true") {
-//     this.total += this.whiteSauce
-//   }
-//   for (i=0; i<addTops; i++)
-//     if()
+  for (i=0; i<3; i++) {
+    if(document.forms[0].meat[i].checked === true);
+      this.total += this.addMeat;
+    }
+    console.log(this.total)
+    return PizzaPrice
+}
+
+
 
 
 //   return total
